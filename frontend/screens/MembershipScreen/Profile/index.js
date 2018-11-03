c83ddf0504b1bda84a936735c2d2c5ba118c3de0
mdjuniooor <mmdsds@naver.com>
2018-11-03 22:21:25 +0900

    stamp 추가

--- frontend/screens/MembershipScreen/Profile/index.js
+++ frontend/screens/MembershipScreen/Profile/index.js
@@ -15,36 +15,61 @@ import {
   Text,
   Body,
   Thumbnail,
-  Left
+  Left,
+  Icon
 } from "native-base";
+import Colors from "../../../constants/Colors";
+
+const BeerStamp = ({ stamp }) => {
+  return (
+    <CardItem>
+      {[...Array(stamp)].map((e, i) => {
+        return (
+          <View style={{marginRight:10}} key={i}>
+            <Icon style={{ fontSize: 30 }} color={Colors.tintColor} name="beer" />
+          </View>
+        );
+      })}
+    </CardItem>
+  );
+};
 
 class Profile extends React.Component {
+  state = {
+    stamp: 5
+  };
   render() {
     return (
       <Container>
-        <Header transparent/>
+        <Header transparent />
         <Content padder>
           <Card>
             <CardItem>
               <Left>
-                <Thumbnail source={require('../../../assets/images/user.jpg')} />
+                <Thumbnail
+                  source={require("../../../assets/images/user.jpg")}
+                />
                 <Body>
-                  <Text>이름</Text>
-                  <Text note>문대성</Text>
+                  <Text>문대성</Text>
+                  <Text note>Beer Geek</Text>
                 </Body>
               </Left>
             </CardItem>
             <CardItem bordered>
               <Body>
                 <Text>
-                  NativeBase is a free and open source framework that enable
-                  developers to build high-quality mobile apps using React
-                  Native iOS and Android apps with a fusion of ES6.
+                  The greatest danger for most of us is not that our aim is too
+                  high and we miss it, but that it is too low and we reach it.
                 </Text>
               </Body>
             </CardItem>
-            <CardItem footer bordered>
-              <Text>GeekyAnts</Text>
+          </Card>
+          <Card>
+            <CardItem>
+              <Text> 스탬프 현황 </Text>
+            </CardItem>
+            <CardItem>
+              <BeerStamp stamp={this.state.stamp} />
             </CardItem>
           </Card>
         </Content>
