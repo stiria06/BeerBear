81c8ae268b2d2bb48526d05af00129d90330c0eb
Zunwu <44821288+Zunwu@users.noreply.github.com>
2018-12-09 13:58:40 +0900

    Update test.js

--- frontend/screens/ShopSearchScreen/test.js
+++ frontend/screens/ShopSearchScreen/test.js
@@ -1,2 +1,7 @@
+import {TextInput} from 'react-native'
+import React from 'react'
+import 
+
+
 <MapView style={styles.map} showsUserLocation followsUserLocation initialRegion:{}/>
 <TextInput style={{}} placeholder='' onSubmitEditting={(text) => this.setState({text}) } />
