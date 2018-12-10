fec0437d513f28b8e127f26f1d3fb13da243a2d2
Zunwu <44821288+Zunwu@users.noreply.github.com>
2018-12-10 13:09:31 +0900

    Update index.js

--- frontend/screens/BuddyMatchingScreen/index.js
+++ frontend/screens/BuddyMatchingScreen/index.js
@@ -1,1 +1,62 @@
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
+import TabBarIcon from "../../components/TabBarIcon";
+import Colors from "../../constants/Colors";
+import FriendsMatch from "./Matching";
+import Chat from "./Chat";
+import FriendsList from "./List";
 
+const BeerSearchTabs = createBottomTabNavigator(
+  {
+    FriendsMatch: {
+      screen: FriendsMatch,
+      navigationOptions: {
+        title: "Find Friends",
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-search" : "md-search"}
+          />
+        )
+      }
+    },
+    Chat: {
+      screen: Chat,      navigationOptions: {
+        title: "Chat List",
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-heart" : "md-heart"}
+          />
+        )
+      }
+    },
+    FriendsList: {
+      screen: FriendsList,
+      navigationOptions: {
+        title: "Matched Friends",
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-heart" : "md-heart"}
+          />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.tintColor
+    }
+  }
+);
+
+export default createStackNavigator(
+  { BeerSearchTabs }, 
+  { 
+    headerMode: "none" ,
+  });
