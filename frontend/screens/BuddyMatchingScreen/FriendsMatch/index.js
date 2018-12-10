79cc3549eaedb63c2ebd028acf8c0df4e37a4576
Zunwu <44821288+Zunwu@users.noreply.github.com>
2018-12-10 13:37:29 +0900

    Update index.js

--- frontend/screens/BuddyMatchingScreen/FriendsMatch/index.js
+++ frontend/screens/BuddyMatchingScreen/FriendsMatch/index.js
@@ -1,1 +1,10 @@
 //실행되면 바로 찾게하기?
+import React from 'react;
+import {StyleSheet, View, Text, } from 'react-native';
+
+const FriendsMatch extends React.Component{
+  render(){
+    return({
+    })
+  }
+}
