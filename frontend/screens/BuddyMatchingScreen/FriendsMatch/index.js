f0c3bf13e250f105e10d66a02c492644647e4720
Zunwu <44821288+Zunwu@users.noreply.github.com>
2018-12-11 12:45:00 +0900

    Update index.js

--- frontend/screens/BuddyMatchingScreen/FriendsMatch/index.js
+++ frontend/screens/BuddyMatchingScreen/FriendsMatch/index.js
@@ -11,17 +11,20 @@ import friends from './assets/friends'
 
 const styles = StyleSheet.create({
   friend: {
+    flex:1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
   avatar: {
+    flex:1,
     margin: 10,
     width: 50,
     height: 50,
     borderRadius: 25,
   },
   name: {
+    flex:1,
     fontSize: 18,
     color: '#000',
   }
@@ -34,8 +37,8 @@ export default class Friend extends Component {
     let lname = friends[0].address;
     return (
       <View style={styles.friend}>
-        <Image style={styles.avatar} source={{ uri: {photo}  }} />
-        <Text style={styles.name}>fname lname</Text>
+        <Image style={styles.avatar} source={{uri:photo}} />
+        <Text style={styles.name}>{fname} {lname}</Text>
       </View>
     );
   }
